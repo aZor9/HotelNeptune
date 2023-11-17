02769035651e23f7635073b388164ed96c430b72
Hugo <hugo.lembrez@gmail.com>
2023-11-17 17:32:59 +0100

    create index

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>Exemple</title>
+		<meta charset="UTF-8">
+    </head>
+    <body>
+        <?php 
+          include("include.php"); 
+        ?>
+    </body>
+</html>
