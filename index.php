69103da9ebdd98db41d41a4cd0c8d10a62c44c04
Hugo <hugo.lembrez@gmail.com>
2023-11-23 15:28:16 +0100

    Page HTML

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
-<html>
-    <head>
-        <title>Exemple</title>
-		<meta charset="UTF-8">
-    </head>
-    <body>
-        <?php 
-          include("include.php"); 
-        ?>
-    </body>
-</html>
+<?php
+
+
+
+?>
+
+<?php include __DIR__ . '/../_includes/document_start.php'; ?>
+
+<div class="container">
+    <h1>Hôtel Neptune</h1>
+</div>
+
+<?php include __DIR__ . '/../_includes/document_end.php'; ?>
