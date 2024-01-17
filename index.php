bafb9b8273264d53981c20dda90f9c9262225a2e
Hugo <hugo.lembrez@gmail.com>
2024-01-17 14:52:09 +0100

    Grosse MAJ

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+
+
+?>
+
+<?php include __DIR__ . '/../_includes/document_start.php'; ?>
+
+<div class="container">
+    <h1>Hôtel Neptune</h1>
+</div>
+<a href='test.php'>test</a>
+<a href='index.html'>index html</a>
+
+<?php include __DIR__ . '/../_includes/document_end.php'; ?>
