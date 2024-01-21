cd9170130652d534875da7354839de3d544cd0da
Raaxun <115032277+Raaxun@users.noreply.github.com>
2024-01-21 16:54:53 +0100

    Revert "sorry not sorry"
    This reverts commit ec5d7d59882e7c1fd708b0d889993945645f2d29.

--- data/index.php
+++ data/index.php
@@ -5,138 +5,154 @@
     <link rel="stylesheet" href="index.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Oswald&family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Oswald&family=Roboto:wght@400;700&display=swap"
+        rel="stylesheet">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@500&display=swap" rel="stylesheet">
 </head>
 
-
+<div class="test">
 <header>
-    <?php
-    include('navbar.php');
-    ?>
+<?php
+    include ('navbar.php');
+?>
+</div>
 </header>
 
-
 <body>
-    <img src="../image/neptunecielv2.jpg" alt="enzo">
-    <section class="BHN">
-        <img src="../image/HotelBHN.jpg" alt="Hôtel Neptune photo" id="Hotel">
-        <div class="text">
-            <h1>Bienvenue à l'Hôtel Neptune</h1>
-            <p>
-                Découvrez l'élégance ultime et le confort incomparable à l'Hôtel Neptune, une oasis de luxe
-                nichée
-                au
-                cœur d'un paysage spectaculaire.
-
-                Que vous voyagiez pour affaires ou pour le plaisir, notre hôtel offre une
-                expérience inoubliable alliant sophistication, style et service exceptionnel.
-            </p>
-        </div>
-    </section>
-    <section class="AurelienTaMaman">
-        <div class="displayfr">
-            <div class="CP">
-                <div id="HotelCP">
-                    <img src="../image/HotelCP.jpeg" alt="Vu de l'Hôtel">
-                </div>
-                <div class="overlay">
-                    <h1>Un Cadre Paradisiaque</h1>
+    
+    
+        <div class="HP">
+            <div class="BHN">
+                <img src="../image/HotelBHN.jpg" alt="Hôtel Neptune photo" id="HotelBHN">
+                <div class="text">
+                    <h1>Bienvenue à l'Hôtel Neptune</h1>
                     <p>
-                        Entouré par des vues à couper le souffle sur l'océan et des jardins luxuriants, l'Hôtel
-                        Neptune
-                        vous
-                        transporte
-                        dans un monde de tranquillité et de beauté.
-                        <br>
-                        Chaque coin de l'établissement respire l'harmonie avec la nature,
-                        créant ainsi une atmosphère sereine et apaisante.
-                    </p>
-                </div>
-
-            </div>
-            <div class="HL">
-                <div id="HotelHL">
-                    <img src="../image/HotelHL.jpeg" alt="Photo Hôtel salle à manger">
-                </div>
-                <div class="overlay">
-                    <h1>Hébergement de Luxe</h1>
-                    <p>
-                        Nos chambres et suites somptueuses sont conçues pour vous offrir le summum du confort.
-                        Des installations modernes et des touches de luxe s'associent pour créer un espace
-                        intime où
-                        vous
-                        pourrez
-                        vous
-                        détendre et vous
-                        ressourcer après une journée bien remplie.
+                        Découvrez l'élégance ultime et le confort incomparable à l'Hôtel Neptune, une oasis de luxe
+                        nichée
+                        au
+                        cœur d'un
+                        paysage spectaculaire.
+                        
+                        Que vous voyagiez pour affaires ou pour le plaisir, notre hôtel offre une
+                        expérience inoubliable alliant sophistication, style et service exceptionnel.
                     </p>
                 </div>
             </div>
-        </div>
-        <div class="displayfr">
-            <div class="GR">
-                <div id="HotelGR">
-                    <img src="../image/HotelGR.jpeg" alt="Gastronomie de l'Hôtel">
-                </div>
-                <div class="overlay">
-                    <h1>Gastronomie Raffinée</h1>
-                    <p>
-                        Laissez-vous séduire par notre cuisine exquise, préparée par des chefs talentueux qui
-                        mettent en
-                        avant
-                        des
-                        saveurs locales et internationales.
-                        <br>
-                        Notre restaurant primé propose un mélange délicieux de plats créatifs et de
-                        classiques revisités, le tout accompagné d'une sélection de vins exceptionnels.
-                    </p>
+            <div class="displayfr">
+                <div class="CP">
+                    <div id="HotelCP">
+                        <img src="../image/HotelCP.jpeg" alt="Vu de l'Hôtel">
+                    </div>
+                    <div class="overlay">
+                        <div class="text">
+                            <h1>Un Cadre Paradisiaque</h1>
+                            <p>
+                                Entouré par des vues à couper le souffle sur l'océan et des jardins luxuriants, l'Hôtel
+                                Neptune
+                                vous
+                                transporte
+                                dans un monde de tranquillité et de beauté.
+                                <br>
+                                Chaque coin de l'établissement respire l'harmonie avec la nature,
+                                créant ainsi une atmosphère sereine et apaisante.
+                            </p>
+                        </div>
+                    </div>
 
                 </div>
-            </div>
-            <div class="BED">
-                <div id="HotelBED">
-                    <img src="../image/HotelBED.jpeg" alt="Espace détente de l'Hôtel">
+                <div class="HL">
+                    <div id="HotelHL">
+                        <img src="../image/HotelHL.jpeg" alt="Photo Hôtel salle à manger">
+                    </div>
+                    <div class="overlay">
+                        <div class="text">
+                            <h1>Hébergement de Luxe</h1>
+                            <p>
+                                <a href="chambres.php" style="color:blue;">Nos chambres</a> et suites somptueuses sont conçues pour vous offrir le summum du confort.
+                                <br>
+                                Des installations modernes et des touches de luxe s'associent pour créer un espace
+                                intime où
+                                vous
+                                pourrez
+                                vous
+                                détendre et vous
+                                ressourcer après une journée bien remplie.
+                            </p>
+                        </div>
+                    </div>
                 </div>
-                <div class="overlay">
-                    <h1>Bien-Être et Détente</h1>
+            </div>
+            <div class="displayfr">
+                <div class="GR">
+                    <div id="HotelGR">
+                        <img src="../image/HotelGR.jpeg" alt="Gastronomie de l'Hôtel">
+                    </div>
+                    <div class="overlay">
+                        <div class="text">
+                            <h1>Gastronomie Raffinée</h1>
+                            <p>
+                                Laissez-vous séduire par notre cuisine exquise, préparée par des chefs talentueux qui
+                                mettent en
+                                avant
+                                des
+                                saveurs locales et internationales.
+                                <br>
+                                Notre restaurant primé propose un mélange délicieux de plats créatifs et de
+                                classiques revisités, le tout accompagné d'une sélection de vins exceptionnels.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="BED">
+                    <div id="HotelBED">
+                        <img src="../image/HotelBED.jpeg" alt="Espace détente de l'Hôtel">
+                    </div>
+                    <div class="overlay">
+                        <div class="text">
+                            <h1>Bien-Être et Détente</h1>
+                            <p>
+                                Profitez d'un moment de détente absolu dans notre spa exclusif, où des thérapeutes
+                                expérimentés
+                                vous
+                                offriront
+                                des soins apaisants pour revitaliser votre esprit, votre corps et votre âme.
+                                <br>
+                                Notre centre de remise en forme
+                                ultramoderne est également à votre disposition pour maintenir votre bien-être physique.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="EI" style="display: flex; max-width:700px;">
+                <div class="text">
+                    <h1>Événements Inoubliables</h1>
                     <p>
-                        Profitez d'un moment de détente absolu dans notre spa exclusif, où des thérapeutes
-                        expérimentés
-                        vous
-                        offriront
-                        des soins apaisants pour revitaliser votre esprit, votre corps et votre âme.
-                        <br>
-                        Notre centre de remise en forme
-                        ultramoderne est également à votre disposition pour maintenir votre bien-être physique.
+                        L'Hôtel Neptune est l'endroit idéal pour organiser des événements mémorables.
+                        Nos espaces élégants et
+                        polyvalents peuvent accueillir des réunions d'affaires, des mariages somptueux et des
+                        célébrations
+                        spéciales, le
+                        tout orchestré avec une attention personnalisée aux détails.
                     </p>
                 </div>
             </div>
-        </div>
-    </section>
-    <section class="Enzopoulet">
-        <h1>Événements Inoubliables</h1>
-        <p>
-            L'Hôtel Neptune est l'endroit idéal pour organiser des événements mémorables.
-            Nos espaces élégants et
-            polyvalents peuvent accueillir des réunions d'affaires, des mariages somptueux et des
-            célébrations
-            spéciales, le
-            tout orchestré avec une attention personnalisée aux détails.
-        </p>
-        <p>
-            Réservez votre séjour dès maintenant et plongez dans une expérience inégalée à l'Hôtel Neptune.
+            <div class="text" style="display: flex; max-width:700px; text-align:center; color:blue;">
 
-            Nous sommes impatients de vous accueillir dans notre monde de luxe, de charme et d'hospitalité
-            exceptionnelle.
-        </p>
-        
-        <div>
-            <a href="chambres.php" id="reservationacceuil">Obtenir une Réservation</a>
+                Réservez votre séjour dès maintenant et plongez dans une expérience inégalée à l'Hôtel Neptune.
+
+                Nous sommes impatients de vous accueillir dans notre monde de luxe, de charme et d'hospitalité
+                exceptionnelle.
+                </p>
+            </div>
+            <div>
+                <a href="chambres.php" id="reservationacceuil">Obtenir une Réservation</a>
+            </div>
         </div>
-    </section>
+   
+
 </body>
 
 </html>
